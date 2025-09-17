47b53fba48b454dad0d90d9c445e4552a7eca858
Dhanashri Devale <dhanashridevale80@gmail.com>
2025-09-17 13:30:38 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -1,8 +1,8 @@
 export const contributorsData = [
   {
-    name: "Siddesh Navthale",
-    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
-    githubUrl: "https://github.com/sidxhdev",
+    name: "Dhanashri Devale",
+    photoUrl: "https://avatars.githubusercontent.com/u/220603077?s=400&u=273cbdaa357dee424dce2d447dab10d39d510d80&v=4",
+    githubUrl: "https://github.com/DhanashriDevale",
   },
   
  
